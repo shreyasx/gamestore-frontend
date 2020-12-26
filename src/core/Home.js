d79c5afd0d6a32faa77d3a9899fe0aafeb02bb77
Shreyas <shreyxs@gmail.com>
2020-12-26 18:50:44 +0530

    adding filter by category

--- src/core/Home.js
+++ src/core/Home.js
@@ -26,6 +26,7 @@ export default function Home() {
 
 	return (
 		<Base title="Home Page" description="Welcome to our Gaming Store!">
+			<h1 className="text-white">Our Games Collection</h1>
 			<div className="row text-center">
 				{error ? error : ""}
 				{loading ? (
@@ -39,7 +40,23 @@ export default function Home() {
 					</>
 				) : (
 					<>
-						<h1 className="text-white">Our Games Collection</h1>
+						<h2 style={{ margin: "20px" }} class="text-white">
+							{/* {"Pick a Category: "}&nbsp;&nbsp;&nbsp;
+							<input type="radio" id="male" name="gender" value="male" />
+							<label for="male">Male</label>&nbsp;&nbsp;&nbsp;
+							<input type="radio" id="female" name="gender" value="female" />
+							<label for="female">Female</label>&nbsp;&nbsp;&nbsp;
+							<input type="radio" id="other" name="gender" value="other" />
+              <label for="other">Other</label> 
+              OPTION 2:
+							{"Pick a Category: "}&nbsp;&nbsp;&nbsp;
+							<button className="btn btn-outline-success">Male</button>
+							&nbsp;&nbsp;
+							<button className="btn btn-outline-success">Male</button>
+							&nbsp;&nbsp;
+							<button className="btn btn-outline-success">Male</button>
+							&nbsp;&nbsp; */}
+						</h2>
 						<div className="row">
 							{products.map((product, index) => {
 								return (
